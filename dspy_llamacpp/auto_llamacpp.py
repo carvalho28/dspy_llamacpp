8c38bd027d7daa02f0a0c36edb05b2918c91a21a
carvalho28 <carvalhoo287@gmail.com>
2025-06-11 18:26:48 +0200

    fix: adapter?

--- dspy_llamacpp/auto_llamacpp.py
+++ dspy_llamacpp/auto_llamacpp.py
@@ -41,7 +41,7 @@ class AutoLlamaCpp:
         )
         # dspy.configure(lm=self.lm)
         if is_reasoning:
-            dspy.configure(lm=self.lm, adapter=dspy.TwoStepAdapter(self.lm))
+            dspy.configure(adapter=dspy.TwoStepAdapter(self.lm))
         else:
             dspy.configure(lm=self.lm)
 
